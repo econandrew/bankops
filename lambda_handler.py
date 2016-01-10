874ded78b0c76a98dc4374b226b06dd0a5d9b59d
Andrew Whitby <me@andrewwhitby.id.au>
2016-01-09 22:33:44 -0500

    install more

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,15 +2,17 @@ from subprocess import call
 import sys
 
 def lambda_handler(event, context):
-    call(["pip", "install", "requests"])
-    call(["pip", "install", "lxml"])
+    call(["sudo yum -y install gcc"], stdout = sys.stdout, shell=True)
+    call(["sudo yum -y install git"], stdout = sys.stdout, shell=True)
+    call(["pip", "install", "requests"], stdout = sys.stdout)
+    call(["pip", "install", "lxml"], stdout = sys.stdout)
     call(["git", "pull"], stdout = sys.stdout)
     call(["rm data/*.xml"], shell=True)
-    call(["rm jekyll/notifications/*.md"], shell=True)
+    call(["rm jekyll/notifications/*.md"], stdout = sys.stdout, shell=True)
     import scraper.eConsult2Github
-    call(["git", "commit", "-a", "-m" "'Automatic refresh'"])
-    call(["git", "push", "origin", "master"])
-    call(["git", "subtree", "push", "--prefix", "jekyll", "origin", "gh-pages"])
+    call(["git", "commit", "-a", "-m" "'Automatic refresh'"], stdout = sys.stdout)
+    call(["git", "push", "origin", "master"], stdout = sys.stdout)
+    call(["git", "subtree", "push", "--prefix", "jekyll", "origin", "gh-pages"], stdout = sys.stdout)
     return 0
     
 if __name__ == "__main__":
