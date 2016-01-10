a69220aef297a1e4a87e25946ee74527fae132b5
Andrew Whitby <me@andrewwhitby.id.au>
2016-01-09 22:05:52 -0500

    Fixing folders

---
+++ lambda_handler.py
@@ -0,0 +1,10 @@
+from subprocess import call
+import sys
+
+call(["git", "pull"], stdout = sys.stdout)
+call(["rm data/*.xml"], shell=True)
+call(["rm jekyll/notifications/*.md"], shell=True)
+import scraper.eConsult2Github
+call(["git", "commit", "-a", "-m" "'Automatic refresh'"])
+call(["git", "push", "origin", "master"])
+call(["git", "subtree", "push", "--prefix", "jekyll", "origin", "gh-pages"])
