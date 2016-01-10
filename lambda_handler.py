1fc80afa9b9e14cb125202ed7c3f5c4be0a10fbd
Andrew Whitby <me@andrewwhitby.id.au>
2016-01-09 22:16:50 -0500

    adding deploy zip

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,12 +1,13 @@
 from subprocess import call
 import sys
 
-call(["pip", "install", "requests"])
-call(["pip", "install", "lxml"])
-call(["git", "pull"], stdout = sys.stdout)
-call(["rm data/*.xml"], shell=True)
-call(["rm jekyll/notifications/*.md"], shell=True)
-import scraper.eConsult2Github
-call(["git", "commit", "-a", "-m" "'Automatic refresh'"])
-call(["git", "push", "origin", "master"])
-call(["git", "subtree", "push", "--prefix", "jekyll", "origin", "gh-pages"])
+def lambda_handler:
+    call(["pip", "install", "requests"])
+    call(["pip", "install", "lxml"])
+    call(["git", "pull"], stdout = sys.stdout)
+    call(["rm data/*.xml"], shell=True)
+    call(["rm jekyll/notifications/*.md"], shell=True)
+    import scraper.eConsult2Github
+    call(["git", "commit", "-a", "-m" "'Automatic refresh'"])
+    call(["git", "push", "origin", "master"])
+    call(["git", "subtree", "push", "--prefix", "jekyll", "origin", "gh-pages"])
