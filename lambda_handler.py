e2b3e6eb4f0a3144e720fe6e5ace917ba87d8b27
Andrew Whitby <me@andrewwhitby.id.au>
2016-01-11 13:13:59 -0500

    Removing lambda stuff

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,20 +1,15 @@
 from subprocess import call
 import sys
 
-def lambda_handler(event, context):
-    call(["sudo yum -y install libxml2-devel libxslt-devel"], stdout = sys.stdout, shell=True)
-    call(["sudo yum -y install gcc"], stdout = sys.stdout, shell=True)
-    call(["sudo yum -y install git"], stdout = sys.stdout, shell=True)
-    call(["sudo pip install requests"], stdout = sys.stdout, shell=True)
-    call(["sudo pip install lxml"], stdout = sys.stdout, shell=True)
-    call(["git", "pull"], stdout = sys.stdout)
-    call(["rm data/*.xml"], shell=True)
-    call(["rm jekyll/notifications/*.md"], stdout = sys.stdout, shell=True)
-    import scraper.eConsult2Github
-    call(["git", "commit", "-a", "-m" "'Automatic refresh'"], stdout = sys.stdout)
-    call(["git", "push", "origin", "master"], stdout = sys.stdout)
-    call(["git", "subtree", "push", "--prefix", "jekyll", "origin", "gh-pages"], stdout = sys.stdout)
-    return 0
-    
-if __name__ == "__main__":
-    lambda_handler(None, None)
+# call(["sudo yum -y install libxml2-devel libxslt-devel"], stdout = sys.stdout, shell=True)
+# call(["sudo yum -y install gcc"], stdout = sys.stdout, shell=True)
+# call(["sudo yum -y install git"], stdout = sys.stdout, shell=True)
+# call(["sudo pip install requests"], stdout = sys.stdout, shell=True)
+# call(["sudo pip install lxml"], stdout = sys.stdout, shell=True)
+call(["git", "pull"], stdout = sys.stdout)
+call(["rm data/*.xml"], shell=True)
+call(["rm jekyll/notifications/*.md"], stdout = sys.stdout, shell=True)
+import scraper.eConsult2Github
+call(["git", "commit", "-a", "-m" "'Automatic refresh'"], stdout = sys.stdout)
+call(["git", "push", "origin", "master"], stdout = sys.stdout)
+call(["git", "subtree", "push", "--prefix", "jekyll", "origin", "gh-pages"], stdout = sys.stdout)
